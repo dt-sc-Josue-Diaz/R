3b744698ece7f42af70a2c71cb1077e2d89b4351
dt-sc-Josue-Diaz <97265914+dt-sc-Josue-Diaz@users.noreply.github.com>
2022-05-20 22:53:56 -0500

    ggplot
    Se agregan graficas con la base de datos enoe

--- 03_ggplot/03_ggplot.R
+++ 03_ggplot/03_ggplot.R
@@ -4,8 +4,43 @@
 # install.packages('tidyverse')
 
 
-# cargar libreria 
+# Cargar librerias 
 library(tidyverse)
 library(readxl)
 
+
+# seccion de trabajo
+setwd("C:/Users/luisj/Dropbox/Ciencia_de_datos/Portafolio/R/03_ggplot")
+
+# Datos datos excel. Base de datos del inegi, muestreo de los ingresos 
+# de las personas
+
 enoe <- read_xlsx("mu_enoe.xlsx")
+
+# Conocer el tipo de datos.
+class(enoe)
+# Obsevaciones vs variables
+dim(enoe)
+
+#nombres de las  variables
+colnames(enoe)
+
+# Gráficas en ggplot
+
+# En este caso estamos viendo si hay correlacion entre años de escolaridad 
+# con el ingreso mensual. 
+
+ggplot( data = enoe) + 
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual))
+
+# modificaiones
+
+ggplot( data = enoe) + 
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual), color = "blue")
+
+# Mapeos estéticos
+# identificar empleos formales contra informales por medio de color.
+
+ggplot( data = enoe) + 
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual, color = tipo_empleo))
+
