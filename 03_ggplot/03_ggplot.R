7d5493544caf05868c83ed4bb43df59f38f8d100
dt-sc-Josue-Diaz <97265914+dt-sc-Josue-Diaz@users.noreply.github.com>
2022-05-20 23:40:38 -0500

    Update 03_ggplot.R
    Secciones con facetas y ggplot por capas

--- 03_ggplot/03_ggplot.R
+++ 03_ggplot/03_ggplot.R
@@ -1,4 +1,4 @@
-# ggplot2
+# ggplot2, básicos.
 
 # Instalacion de la libreria 
 # install.packages('tidyverse')
@@ -44,3 +44,62 @@ ggplot( data = enoe) +
 ggplot( data = enoe) + 
   geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual, color = tipo_empleo))
 
+# identificar empleos formales contra informales por medio de formas distintas
+
+ggplot( data = enoe) + 
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual, shape = tipo_empleo))
+
+
+# Comparamos a años de escolaridad vs edad. Identificamos ahora por medio 
+# del ingreso mensual por una escala de transparencias. Esta no es una buena
+# gráfica. 
+
+# ggplot( data = enoe) + 
+#  geom_point(mapping = aes(x = anios_esc, y = edad, alpha = ingreso_mensual))
+
+# ggplot trabaja en capas
+ggplot( data = enoe) + # capa 1
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual), color = "blue") + #capa 2
+  geom_smooth(aes(anios_esc,ingreso_mensual)) # tercera capa. 
+
+
+# Separar por facetas.
+
+# facet_wrap. Esta gráfica nos separa por sexo los datos años de escolaridad
+# contra ingreso mensual, identificada por tipo de empleo por color. 
+ggplot( data = enoe) + 
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual, color = tipo_empleo)) +
+  facet_wrap(~sex, nrow=1, ncol=2)
+
+# facet_grid
+ggplot( data = enoe) + 
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual, color = tipo_empleo)) +
+  facet_grid(tipo_empleo~sex)
+
+# Comparemos dos datos con dos tipos distintos de gráficos
+# Gráfico 1
+ggplot( data = enoe) + 
+  geom_point(mapping = aes(x = anios_esc, y = ingreso_mensual), color = "blue")
+# Gráfico 2
+ggplot( data = enoe) + 
+  geom_smooth(mapping = aes(x = anios_esc, y = ingreso_mensual), color = "blue")
+
+# Tambien podemos separar una gráfica de curvas
+ggplot( data = enoe) + 
+  geom_smooth(mapping = aes(x = anios_esc, y = ingreso_mensual, linetype = sex))
+# Sin embargo no todas las modificaciones se pueden hacer.
+
+# Combinar gráificas. 
+ggplot( data = enoe, mapping = aes(x = anios_esc, y = ingreso_mensual)) + 
+  geom_smooth( mapping = aes(linetype = tipo_empleo)) +
+  geom_point(mapping = aes(color = tipo_empleo))
+
+# Esta gráfica es importante, ya que nos puede resultar engañoso la tendencia de que
+# entre mas años de escolaridad mayor ingreso mensual.
+
+# Variacion de la grafica.
+ggplot( data = enoe, mapping = aes(x = anios_esc, y = ingreso_mensual)) + 
+  geom_smooth(data = filter(enoe, estado == "Jalisco"), se = FALSE) +
+  geom_point(mapping = aes(color = niv_edu), show.legend = FALSE)
+# En esta grafica los puntos son los datos del todo el pais, pero la linea los 
+# datos de Jalisco. 
